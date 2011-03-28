5bdb593203535954b8ed8eab32a73763f5f81bb9
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-03-28 22:56:18 +0400

    Fix paths.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PATH=$HOME/root/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib libbz2.a
-PATH=$HOME/root/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PREFIX=$HOME/root/nacl64 install
+PATH=$HOME/root/nacl-sdk/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib libbz2.a
+PATH=$HOME/root/nacl-sdk/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PREFIX=$HOME/root/nacl-sdk/nacl64/usr install
 
