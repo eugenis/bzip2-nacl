6ecba881e29ee692f047c05990efa4498837a86c
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-03-30 23:09:58 +0400

    Improve build.sh.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-PATH=$HOME/root/nacl-sdk/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib libbz2.a
-PATH=$HOME/root/nacl-sdk/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PREFIX=$HOME/root/nacl-sdk/nacl64/usr install
+ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
+PATH=$ROOT/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib libbz2.a
+PATH=$ROOT/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PREFIX=$ROOT/nacl64/usr install
