2378da421bf6628b2e423161982ef3be7ae7b482
Evgeniy Stepanov <eugenis@google.com>
2011-09-10 22:41:51 +0400

    Update the arch prefix.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
-PATH=$ROOT/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib libbz2.a
-PATH=$ROOT/bin:$PATH make CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PREFIX=$ROOT/nacl64/usr install
+PATH=$ROOT/bin:$PATH make CC=x86_64-nacl-gcc AR=x86_64-nacl-ar RANLIB=x86_64-nacl-ranlib libbz2.a
+PATH=$ROOT/bin:$PATH make CC=x86_64-nacl-gcc AR=x86_64-nacl-ar RANLIB=x86_64-nacl-ranlib PREFIX=$ROOT/x86_64-nacl/usr install
